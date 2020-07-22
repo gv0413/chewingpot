2a399ac1055ad5151c3595160387b8b7d28e0d8c
gv0413 <ggvv0413@gmail.com>
2020-07-23 01:02:14 +0900

    add SearchForm

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <template>
   <div>
+    <SearchForm/>
   </div>
 </template>
 
 <script>
+import SearchForm from './components/SearchForm'
 
 export default {
+  components: {
+    SearchForm
+  }
 }
 </script>
 
