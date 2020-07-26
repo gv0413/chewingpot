2e46dfe3a0b6dab033d7618894fdbad466ecb0c5
gv0413 <ggvv0413@gmail.com>
2020-07-26 18:38:04 +0900

    [enc-009] Add restaurant menu component into Restaurant page

--- src/components/review/ReviewCard.vue
+++ src/components/review/ReviewCard.vue
@@ -4,7 +4,8 @@
     썸네일 이미지 <br> <img :src="reviewInfo.video_thumbnail_url" alt="" width="100px" height="100px"> <br>
     채널 썸네일 이미지 <br> <img :src="reviewInfo.channel_thumbnail_url" alt="" width="100px" height="100px"> <br>
     제목 ㅣ{{reviewInfo.title}}<br>
-    채널명 ㅣ{{reviewInfo.channel_name}}<br> <br>
+    채널명 ㅣ{{reviewInfo.channel_name}}<br>
+    <br>
   </div>
 </template>
 
