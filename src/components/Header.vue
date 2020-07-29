e015e4f5dfbf9b466fdcea5fec3bc5cf2411b738
gv0413 <ggvv0413@gmail.com>
2020-07-29 18:51:43 +0900

    [ENC-40] [ENC-42] get youtube review info, show youtube iframe into RestaurantReview page #27

---
+++ src/components/Header.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>restaurant name</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/components/review/RestaurantReview.vue
+++ src/components/review/RestaurantReview.vue
@@ -1,13 +1,53 @@
 <template>
   <div>
     리뷰 상세 페이지
+    <Header/>
+    <iframe :src="`https://www.youtube.com/embed/${youtubeId}`" frameborder="0"></iframe>
   </div>
 </template>
 
 <script>
-export default {
+import Header from '../Header';
+import axios from 'axios';
 
+export default {
+  components: {
+    Header,
+  },
+  data: function() {
+    return {
+      reviewInfo: [],
+      youtubeUrl: [],
+      youtubeId: '',
+      isReviewInfoLoaded: false
+    }
+  },
+  computed: {
+    reviewId: function() {
+      return this.$route.params.reviewId
+    }
+  },
+  created() {
+    this.loadReview();
+  },
+  methods: {
+    loadReview : function() {
+      let url = `http://127.0.0.1:3000/video_reviews/${this.reviewId}`;
+      axios.get(url)
+        .then((response) => {
+          this.reviewInfo = response.data.data[0]
+          this.youtubeUrl = this.reviewInfo.youtube_url.split('=')
+          this.youtubeId = this.youtubeUrl[1]
+          this.isReviewInfoLoaded = true
+        })
+        .catch((error) => {
+          console.log(error);
+          this.isReviewInfoLoaded = true
+        })
+    }
+  }
 }
+
 </script>
 
 <style>
