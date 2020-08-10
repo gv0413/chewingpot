44945a7ef26b78016756355af874d2a14c1a517f
mj Ann <ggvv0413@gmail.com>
2020-08-10 23:50:54 +0900

    [ENC-58] Set Home page #52

---
+++ src/components/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <Header></Header>
+    <!-- video  -->
+    <restaurantReview></restaurantReview>
+    <!-- restaurant info  -->
+    <restaurantInfo></restaurantInfo>
+  </div>
+</template>
+
+<script>
+import Header from './Header.vue'
+
+export default {
+  components: {
+    Header,
+  }
+}
+</script>
+
+<style>
+
+</style>
