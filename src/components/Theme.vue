f88f6684a33ba0ad69514aa79d45f8b0bff06918
mj Ann <ggvv0413@gmail.com>
2020-08-15 02:40:16 +0900

    [ENC-54] Add Montserrat font #41

--- src/components/Theme.vue
+++ src/components/Theme.vue
@@ -2,10 +2,10 @@
   <div class="container pt-pb-10 between-component">
     <div class="wrap">
       <p class="mb-05">츄잉픽, NOW!</p>
-      <button class="pr-1 pl-1 mr-05 theme-btn">데이트</button>
-      <button class="pr-1 pl-1 mr-05 theme-btn">몸보신</button>
-      <button class="pr-1 pl-1 mr-05 theme-btn">미슐랭가이드</button>
-      <button class="pr-1 pl-1 mr-05 theme-btn">분식</button>
+      <button class="mr-10px theme-btn">데이트</button>
+      <button class="mr-10px theme-btn">몸보신</button>
+      <button class="mr-10px theme-btn">미슐랭가이드</button>
+      <button class="mr-10px theme-btn">분식</button>
     </div>
   </div>
 </template>
