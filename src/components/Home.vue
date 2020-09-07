69695dbb26999a79106e6201301aef008d3acdd8
mj Ann <ggvv0413@gmail.com>
2020-09-08 01:20:00 +0900

    [ENC-86] control api calls with prevData to prevent infinite calls #56

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,7 +2,7 @@
   <div class="background">
     <Header></Header>
     <Theme @parent="handleEvent"></Theme>
-    <div v-infinite-scroll="loadMore" infinite-scroll-disabled="busy" infinite-scroll-distance="1000">
+    <div v-infinite-scroll="loadMore" infinite-scroll-disabled="busy" infinite-scroll-distance="1000" >
       <div v-for="(reviewInfo, i) in data" :key="i">
         <restaurantReview v-bind:reviewInfo="reviewInfo"></restaurantReview>
         <restaurantInfo v-bind:reviewInfo="reviewInfo"></restaurantInfo>
@@ -32,12 +32,19 @@ export default {
       tpoCategory: '',
       isRestaurantInfoFoldeds: [],
       cursorId: '',
+      prevData: [0,0,0,0,0],
     }
   },
   created() {
   },
   methods: {
     loadMore: function() {
+      console.log(this.cursorId)
+      console.log(this.prevData)
+      if(this.prevData.length==0) {
+        console.log('다봤어, 다음 카테고리로 넘어가')
+        return []
+      }
       this.busy = true
       let url = `http://127.0.0.1:3000/video_reviews?cursorId=${this.cursorId}`;
       if (this.tpoCategory) {
@@ -49,6 +56,7 @@ export default {
             this.data.push(element)
           })
           this.isRestaurantInfoFoldeds = []
+          this.prevData = response.data.data
           this.cursorId = this.data[this.data.length - 1].id
         })
         .catch((error) => {
