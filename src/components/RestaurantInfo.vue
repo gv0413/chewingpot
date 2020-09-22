ed1370d48f81569c6240d077a7e359148f6fdd19
gv0413 <ggvv0413@gmail.com>
2020-09-23 02:06:13 +0900

    [ENC-102] Add restaurant call link #66

--- src/components/RestaurantInfo.vue
+++ src/components/RestaurantInfo.vue
@@ -5,11 +5,22 @@
         <button class="detail-btn" @click="fold"> 식당 정보 보기</button> 
       </div>
       <hr class="border-gry">
-      <div class="pt-pb-10" v-if="isRestaurantInfoOpen">
+      <div class="pt-pb-10" style="position:relative" v-if="isRestaurantInfoOpen">
         <i class="fas fa-utensils mr-1 mb-05"></i>{{reviewInfo.restaurants.name}} ㅣ {{reviewInfo.restaurants.category}}<br>
         <i class="fas fa-map-marker-alt mr-1 mb-05"></i>{{reviewInfo.restaurants.location}}<br>
-        <i class="fas fa-phone-alt mr-1 mb-1"></i>{{reviewInfo.restaurants.contact}}<br>
+        <i class="fas fa-phone-alt mr-1 mb-1" ></i>{{reviewInfo.restaurants.contact}}<br>
         <restaurant-map v-bind:restaurantInfo="reviewInfo.restaurants"></restaurant-map> 
+        <a class="decoration-none"
+            :href="`tel:${reviewInfo.restaurants.contact}`"
+            style="
+                  width: 100% !important;
+                  border: 1px solid black;
+                  display: flex;
+                  flex-basis: 100%;
+                  margin: 8px 0px;
+                  justify-content: center;
+                  padding: 10px 0;"
+            >전화하기</a>
       </div>
     </div>
   </div>
