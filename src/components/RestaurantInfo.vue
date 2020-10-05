c3f4c041974c8f575873f905e8bcf53547bb80e9
mj Ann <ggvv0413@gmail.com>
2020-10-06 02:43:29 +0900

    [ENC-108][ENC-66] Distinguish a closed restaurant #72 #50

--- src/components/RestaurantInfo.vue
+++ src/components/RestaurantInfo.vue
@@ -9,7 +9,13 @@
         <i class="fas fa-utensils mr-1 mb-05"></i>{{reviewInfo.restaurants.name}} ㅣ {{reviewInfo.restaurants.category}}<br>
         <i class="fas fa-map-marker-alt mr-1 mb-05"></i>{{reviewInfo.restaurants.location}}<br>
         <i class="fas fa-phone-alt mr-1 mb-1"></i>{{reviewInfo.restaurants.contact}}<br>
-        <restaurant-map v-bind:restaurantInfo="reviewInfo.restaurants"></restaurant-map> 
+        <div v-if="!reviewInfo.restaurants.is_closed">
+          <restaurant-map v-bind:restaurantInfo="reviewInfo.restaurants"></restaurant-map> 
+        </div>
+        <div v-else class="text-center t-primary background pt-pb-5">
+          <i class="fas fa-exclamation-circle"></i>
+          폐점한 식당입니다.
+        </div>
       </div>
     </div>
   </div>
